62a536477c71d9b3c0c7b32bd6fa456c3206b76a
Roberto A Verdecia Ferras <robertojonan01@gmail.com>
2022-07-23 23:54:28 +0200

    dockerizing the app

---
+++ todos/db_seed.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS todo (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (30) NOT NULL,
+    completed BOOLEAN NOT NULL
+);
--- todos/server/src/db.ts
+++ todos/server/src/db.ts
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool_serv = new Pool({
   user: "me",
-  host: "localhost",
+  host: "postgres",
   database: "todos",
   password: "password",
   port: 5432,
