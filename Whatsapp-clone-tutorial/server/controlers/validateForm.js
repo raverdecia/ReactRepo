7985bcc572095754515fc982effebc972a7aed42
Roberto A Verdecia Ferras <robertojonan01@gmail.com>
2022-07-31 16:53:15 +0200

    using best practices

---
+++ Whatsapp-clone-tutorial/server/controlers/validateForm.js
@@ -0,0 +1,23 @@
+const Yup = require("yup");
+
+const formSchema = Yup.object({
+  username: Yup.string().required("Username require").min(6, "Username to short!").max(28, "Username to long!"),
+  password: Yup.string().required("Password require").min(6, "Password to short!").max(28, "Password to long!"),
+});
+
+const validateForm = (req, res) => {
+  const formData = req.body;
+  formSchema
+    .validate(formData)
+    .catch((err) => {
+      res.status(422).send();
+      console.log(err.errors);
+    })
+    .then((valid) => {
+      if (valid) {
+        console.log("form is good");
+      }
+    });
+};
+
+module.exports = validateForm;
--- Whatsapp-clone-tutorial/server/routers/authRouter.js
+++ Whatsapp-clone-tutorial/server/routers/authRouter.js
@@ -1,40 +1,13 @@
 const express = require("express");
+const validateForm = require("../controlers/validateForm");
 const router = express.Router();
-const Yup = require("yup");
-
-const formSchema = Yup.object({
-  username: Yup.string().required("Username require").min(6, "Username to short!").max(28, "Username to long!"),
-  password: Yup.string().required("Password require").min(6, "Password to short!").max(28, "Password to long!"),
-});
 
 router.post("/login", (req, res) => {
-  const formData = req.body;
-  formSchema
-    .validate(formData)
-    .catch((err) => {
-      res.status(422).send();
-      console.log(err.errors);
-    })
-    .then((valid) => {
-      if (valid) {
-        console.log("form is good");
-      }
-    });
+  validateForm(req, res);
 });
 
 router.post("/signup", (req, res) => {
-  const formData = req.body;
-  formSchema
-    .validate(formData)
-    .catch((err) => {
-      res.status(422).send();
-      console.log(err.errors);
-    })
-    .then((valid) => {
-      if (valid) {
-        console.log("form is good");
-      }
-    });
+  validateForm(req, res);
 });
 
 module.exports = router;
