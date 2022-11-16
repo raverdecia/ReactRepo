0e7df353e2b217295f9d2b022ebfaf40cf91b80a
Roberto A. Verdecia Ferrás <72039197+raverdecia@users.noreply.github.com>
2022-11-16 19:16:20 +0100

    Delete Whatsapp-clone-tutorial directory

--- Whatsapp-clone-tutorial/client/src/socket.js
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-const socket = new io("http://localhost:4000", {
-  autoConnect: false,
-  withCredentials: true,
-});
-
-export default socket;
--- Whatsapp-clone-tutorial/server/redis.js
+++
@@ -1,5 +0,0 @@
-const Redist = require("ioredis");
-
-const redisClient = new Redist();
-
-module.exports = redisClient;
