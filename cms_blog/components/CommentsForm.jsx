69b07aa03dae8b05142738e4b7712f8a7332fc48
Roberto A Verdecia Ferras <robertojonan01@gmail.com>
2022-11-26 10:55:30 +0100

    creating authors and comments

--- cms_blog/components/CommentsForm.jsx
+++ cms_blog/components/CommentsForm.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-const CommentsForm = ({}) => {
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false);
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccesMessage, setShowSuccesMessage] = useState(false);
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
+  useEffect(() => {}, []);
   return (
-    <div>
-      <h1>CommentsForm</h1>
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">CommentsForm</h3>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <textarea
+          ref={commentEl}
+          className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gay-200 bg-gray-100 text-gray-700"
+          placeholder="Comment"
+          name="comment"
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <input
+          type="text"
+          ref={nameEl}
+          className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gay-200 bg-gray-100 text-gray-700"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          type="text"
+          ref={emailEl}
+          className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gay-200 bg-gray-100 text-gray-700"
+          placeholder="Email"
+          name="email"
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-4 mb-4"></div>
     </div>
   );
 };
